dca03a1ecc9d4b68d8ba3c0b0f8b80853ea022fe
Joe Masters <jmasters@nrao.edu>
2013-08-21 14:24:58 -0400

    Added checkbox grid for bank, subband.  Added buffers for each bank.

--- mock_zmq_publisher.py
+++ mock_zmq_publisher.py
@@ -5,30 +5,23 @@ import zmq
 from zmq.eventloop import ioloop
 ioloop.install()
 
-NREQUESTS = 10
+
 NCHANS = 1024 # 32768 is max number of channels with VEGAS
 
 def mock_zmq_publisher(port):
     context = zmq.Context()
-    socket  = context.socket(zmq.REP)
+    socket  = context.socket(zmq.REP)  # request-reply pattern
     socket.bind("tcp://*:%s" % port)
-    
-    print "Running server on port: ", port
-    
-    # serves NREQUESTS requests and dies
-    reqnum = 0
-#    for reqnum in range(NREQUESTS):
-    while True:
 
-        socket.recv()
+    print "Running server on port: ", port
+
+    while True:
+        socket.recv()  # receive any message to trigger sending data
 
         # Wait for next request from client
         data = [random.randrange(0,98765) for i in xrange(NCHANS)]
-        
+
         # the following will eventually be replaced with a protobuf
         socket.send_pyobj(data)
-        # print('sent data',reqnum)
-        # reqnum += 1
 
-        
     socket.send_pyobj('close')
--- vdd_server_mock.py
+++ vdd_server_mock.py
@@ -8,6 +8,8 @@ import tornado.web
 import tornado.ioloop
 
 from multiprocessing import Process
+from collections import deque
+from collections import defaultdict
 import pickle
 import time
 import os
@@ -15,22 +17,19 @@ import sys
 import signal # for timeout
 import subprocess
 
+from mock_zmq_publisher import mock_zmq_publisher
+
+
 UPDATE_INTERVAL = 1 # seconds
 NCHANS = 512  # number of channels for client to display
+ACTIVE_MOCK_BANKS = ['A', 'B']
+BUFFER_SIZE = 600 # num of spectra to store at full res for each bank
+
+PUBLISHER_PORT = {'A': '5551', 'B': '5552', 'C': '5553', 'D': '5554',
+                  'E': '5555', 'F': '5556', 'G': '5557', 'H': '5558'}
 
 def timeout_handler(signum, frame):
-    raise 
-
-from mock_zmq_publisher import mock_zmq_publisher
-PUBLISHER_PORT  = {'A': '5551',
-                   'B': '5552',
-                   'C': '5553',
-                   'D': '5554',
-                   'E': '5555',
-                   'F': '5556',
-                   'G': '5557',
-                   'G': '5558'}
-ACTIVE_MOCK_BANKS = ['A', 'B']
+    raise
 
 def connect_to_bank(bank, bank_url, publisher_socket, client_socket):
     """Connect (i.e. subscribe) to a data publisher.
@@ -42,14 +41,13 @@ def connect_to_bank(bank, bank_url, publisher_socket, client_socket):
     client_socket -- from ZMQSocket class in this file
 
     """
-    
     publisher_socket.connect(bank_url)
 
     def handler(msg):
-        # The mock server uses a pickled python object, while the vegas
-        #  server uses a protobuf
+        # The mock server uses a pickled python object, while the
+        #  vegas server uses a protobuf
         msg = pickle.loads(msg)
-        client_socket.write_message(['data',msg])
+        client_socket.write_message(['data', bank, msg])
 
     publisher_socket.send('')
     message = publisher_socket.recv()
@@ -59,13 +57,17 @@ def connect_to_bank(bank, bank_url, publisher_socket, client_socket):
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html", title = 'Vegas Data Display')
-      
+
 class ZMQWebSocket(websocket.WebSocketHandler):
     def open(self):
         """
         This method is called when the JS creates a WebSocket object.
+
         """
-        self.times = {}
+        def default_factory():
+            return deque(maxlen=BUFFER_SIZE)
+        self.databuffer = defaultdict(default_factory)
+
         self.msgSize = None
         self.nSpecInAve = None
         self.specAve = None # running average of spectra
@@ -74,44 +76,53 @@ class ZMQWebSocket(websocket.WebSocketHandler):
         #  Launch our mock up zmq publishers in separate processes.
         for bb in ACTIVE_MOCK_BANKS:
             print 'Bank',bb,'Port',PUBLISHER_PORT[bb]
-            Process(target=mock_zmq_publisher, args=(PUBLISHER_PORT[bb],)).start()
+            Process(target=mock_zmq_publisher,
+                    args=(PUBLISHER_PORT[bb],)).start()
 
         ctx = zmq.Context()
-        self.publisher_socket = ctx.socket(zmq.REQ)
+        self.publisher_socket = ctx.socket(zmq.REQ) # request-reply pattern
 
         # send message to client about what banks are active
         self.write_message(['bank_config', ACTIVE_MOCK_BANKS]);
-        
+
     def on_message(self, bank):
-        """
-        This method is called when the server responds.  See send call in the
-        onmessage function in Display.js in the client code.
+        """Handle message from client.
+
+        This method is called when the client responds at the end of
+        the bank_config step in Display.js.
 
         """
         print 'Getting data from bank ' + bank
         bank_url = "tcp://localhost:%s" % PUBLISHER_PORT[bank]
 
-        #  NB: we additionally pass in a reference to self (ZMQWebSocket instance).
+        #  NB: we additionally pass a reference to self (ZMQWebSocket instance).
         connect_to_bank(bank, bank_url, self.publisher_socket, self)
 
     def write_message(self, msg):
-        """ 
-        The following extends the write_message() method of the
-        websocket.WebSocketHandler() base class [using super()]
-        with the preamble code that converts the message to unicode,
-        sets the message size and records timing information.
+        """Send a message to the client.
 
-        write_message is invoked by the write_message() call in connect_to_bank()
-        
+        This method extends the write_message() method of the
+        websocket.WebSocketHandler() base class [using super()] with
+        the preamble code that converts the message to unicode, sets
+        the message size and records timing information.
+
+        write_message is invoked by the write_message() call in
+        connect_to_bank() or socket open() on socket creation.
+
         """
 
         if not msg:
             print 'There is no message to write.  Quitting.'
             sys.exit()
         elif 'data' == msg[0]:
-            npmsg = np.array(msg[1])
+            bank = msg[1]
+            npmsg = np.array(msg[2])
+            self.databuffer[bank].append(npmsg)
+
             rebinned_data = [np.mean(x) for x in npmsg.reshape((NCHANS,len(npmsg)/NCHANS))]
-            data = unicode([msg[0], rebinned_data, np.floor(min(rebinned_data)), np.ceil(max(rebinned_data))])
+            data = unicode([msg[0], rebinned_data,
+                            np.floor(min(rebinned_data)),
+                            np.ceil(max(rebinned_data))])
         elif 'bank_config' == msg[0]:
             print repr(msg)
             data = repr(msg)
@@ -123,7 +134,7 @@ class ZMQWebSocket(websocket.WebSocketHandler):
         # the following is an idiom for
         # if x then do y, else no change (set x=x)
         self.msgSize = self.msgSize or len(data) * 2
-               
+
         # the following line sends the data to the JS client
         # python 3 syntax would be super().write_message(data)
         super(ZMQWebSocket, self).write_message(data)
@@ -141,7 +152,7 @@ if __name__ == "__main__":
         (r"/", MainHandler),
         (r"/websocket", ZMQWebSocket)
     ], **settings)
-    
+
     application.listen(8889,'0.0.0.0')
     try:
         tornado.ioloop.IOLoop.instance().start()
