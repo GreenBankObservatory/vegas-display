063a072422927f7343e2e49daf7255faa14d1319
Joe Masters <jmasters@nrao.edu>
2014-04-03 16:02:25 -0400

    More code cleanup

--- vdd_stream_server.py
+++ vdd_stream_server.py
@@ -1,6 +1,6 @@
 import zmq
 from zmq.eventloop import zmqstream
-
+import random
 import numpy as np
 import pylab as pl
 
@@ -8,7 +8,9 @@ from tornado import websocket
 import tornado.web
 import tornado.ioloop
 
+from time import strftime,sleep
 import multiprocessing
+import threading
 from collections import deque
 from collections import defaultdict
 import pickle
@@ -22,10 +24,11 @@ from PBVegasData_pb2 import *
 
 from pprint import pprint
 import pdb
-
+from random import randint
 from DataStreamUtils import get_service_endpoints, get_directory_endpoints
 
-DEBUG = True
+DO_PARSE = True
+DEBUG = False
 LIVETEST = False
 BANK_NUM = {'A':0, 'B':1, 'C':2, 'D':3,
             'E':4, 'F':5, 'G':6, 'H':7}
@@ -47,8 +50,8 @@ class MainHandler(tornado.web.RequestHandler):
 
 class DataRetriever():
     def __init__(self,):
-        self.previous_scan = None
-        self.previous_integration = None
+        self.prev_scan = None
+        self.prev_integration = None
 
     def handle_response(self, manager_response):
         el = len(manager_response)
@@ -72,9 +75,24 @@ class DataRetriever():
                     print df.name, '=', response
                 return response
             else:
-                df = pbVegasData()
-                df.ParseFromString(manager_response[1])
-                #pdb.set_trace()
+                if DO_PARSE:
+                    df = pbVegasData()
+                    t = threading.Thread(target=df.ParseFromString, args=(manager_response[1],))
+                    t.start()
+                    t.join()
+                else:
+                    class DF:
+                        project_id = 'ProjectFOO'
+                        scan_number = random.randint(0,9999)
+                        integration = random.randint(0,9999)
+                        time = 100
+                        cal_state = None
+                        sig_ref_state = None
+                        data_dims = 1024
+                        data = np.random.random(1024)
+
+                    df = DF()
+
                 if DEBUG:
                     print 'project: ', df.project_id
                     print 'scan: ', df.scan_number
@@ -86,9 +104,12 @@ class DataRetriever():
                     print "data[:8]: ", df.data[:8]
 
                 arr = np.array(df.data)
-                dims = df.data_dims[::-1]
-                arr = arr.reshape(dims)
-                spectrum = arr[0,0,:]
+                if DO_PARSE:
+                    dims = df.data_dims[::-1]
+                    arr = arr.reshape(dims)
+                    spectrum = arr[0,0,:]
+                else:
+                    spectrum = arr
     
                 if DEBUG:
                     print 'max in arr', np.max(arr)
@@ -105,40 +126,26 @@ class DataRetriever():
         else:
             return None
 
-    def get_data_sample(self, bank, client_socket):
+    def get_data_sample(self, bank, context, device_url, major_key, minor_key):
         """Connect (i.e. subscribe) to a data publisher.
 
         Arguments:
         bank -- the name of the bank (e.g. 'A')
-        client_socket -- from ZMQSocket class in this file
 
         """
-        context = zmq.Context()
-        if LIVETEST:
-            major_key = "VEGAS"
-            minor_key = "Bank{0}Mgr".format(bank)
-        else:
-            major_key = "VegasTest"
-            minor_key = major_key + bank
-
-        dataKey = "%s.%s:Data" % (major_key, minor_key)
-        stateKey = "%s.%s:P:state" % (major_key, minor_key)
-
-        directory_url = get_directory_endpoints("request")
-        device_url = get_service_endpoints(context, directory_url,
-                                           major_key, minor_key, SERV_SNAPSHOT)
-
-        request_url = context.socket(zmq.REQ)
         if 'NOT FOUND' in device_url:
             print 'Bank', bank, device_url
-            # client_socket.write_message(['error', bank])
             return ['error']
         else:
+            request_url = context.socket(zmq.REQ)
             request_url.connect(device_url)
 
             if DEBUG:
                 print '------------'
     
+            dataKey = "%s.%s:Data" % (major_key, minor_key)
+            stateKey = "%s.%s:P:state" % (major_key, minor_key)
+
             request_url.send(str(stateKey))
             response = request_url.recv_multipart()
             state = self.handle_response(response)
@@ -147,63 +154,135 @@ class DataRetriever():
                 request_url.send(str(dataKey))
                 response = request_url.recv_multipart()
 
+                self.handle_response(response)
                 scan, project, spectrum, integration = self.handle_response(response)
                 if DEBUG:
-                    print "scan {0} ~ prevscan {1} int {2} ~ prevint {3}\n\n".format(scan,self.previous_scan,integration,self.previous_integration)
-                if scan != self.previous_scan or integration != self.previous_integration:
-                    self.previous_scan = scan
-                    self.previous_integration = integration
+                    print "scan {0} ~ prevscan {1} int {2} ~ prevint {3}\n\n".format(scan,
+                                                                                     self.prev_scan,
+                                                                                     integration,
+                                                                                     self.prev_integration)
+                # if something changed
+                if scan != self.prev_scan or integration != self.prev_integration:
+                    self.prev_scan = scan
+                    self.prev_integration = integration
                     return ['ok', spectrum, project, scan, state, integration]
-                else:
-                    print "Scan and integration numbers haven't changed for bank {2}. Integration: {0} Bank: {1}".format(scan,integration,bank)
-#                    return ['same']
-                    return ['ok', spectrum, project, scan, state, integration]
+                else:  # if nothing changed
+                    if DEBUG:
+                        print "Scan and integration numbers haven't changed for ",
+                        print "bank {2}. Integration: {0} Bank: {1}".format(scan,integration,bank)
+                    return ['same', spectrum, project, scan, state, integration]
             else:
                 return ['error']
 
 class ZMQWebSocket(websocket.WebSocketHandler):
+
+    connections = []
+    data = [None]
+    server_thread = []
+    requesting_data = True
+
+    def stop_requesting(self,):
+        self.requesting_data = False
+
+    def run_display_server(self,):
+        """Start up the server that reads spectra from the VEGAS manger
+
+        This server reads from the VEGAS manager and serves it up to
+        the display client.
+
+        """
+
+        def default_factory():
+            return deque(maxlen=BUFFER_SIZE)
+
+        databuffer = defaultdict(default_factory)
+        messageRetriever = DataRetriever()
+
+        msgSize = None
+        nSpecInAve = None
+        specAve = None # running average of spectra
+        publisher_socket = None
+        active_banks = None
+    
+        # send message to client about what banks are active
+        banks = {'A':True, 'B':True, 'C':True, 'D':True, 'E':True, 'F':True, 'G':True, 'H':True}
+
+        directory_url = get_directory_endpoints("request")
+        context = zmq.Context()
+    
+        device_url = {}
+        major_key = {}
+        minor_key = {}
+        for b in banks.keys():
+            if LIVETEST:
+                major_key[b] = "VEGAS"
+                minor_key[b] = "Bank{0}Mgr".format(b)
+            else:
+                major_key[b] = "VegasTest"
+                minor_key[b] = major_key[b] + b
+
+            device_url[b] = get_service_endpoints(context, directory_url,
+                                                  major_key[b], minor_key[b], SERV_SNAPSHOT)
+    
+        while self.requesting_data:
+            all_banks_spectra = []
+            metadata = []
+            first_bank = True
+            for b in banks.keys():
+                if DEBUG:
+                    print 'Requesting data for bank', b
+    
+                if DEBUG:
+                    print 'directory_url', directory_url
+                    print 'device_url', device_url
+
+                response = messageRetriever.get_data_sample(b, context, device_url[b],
+                                                            major_key[b], minor_key[b])
+                if response[0] == 'error':
+                    spectrum = np.zeros(NCHANS)
+                else:
+                    spectrum = response[1]
+                    if first_bank: # only use the metadata from first bank
+                        project, scan, state, integration = response[2:]
+                        if response[0] == 'same':
+                            update_waterfall = 0
+                        else:
+                            update_waterfall = 1
+                        metadata = [project, scan, state, integration, update_waterfall]
+                        first_bank = False
+
+                all_banks_spectra.append(spectrum)
+    
+            if DEBUG:
+                print 'nspectra',len(all_banks_spectra)
+
+            self.data[0] = [metadata, all_banks_spectra]
+            if DEBUG:
+                print strftime("%H:%M:%S")
+            sleep(.300)
+
+
+    def initialize(self):
+        print '\n\nINITIALIZING!!!\n\n'
+
     def open(self):
         """
         This method is called when the JS creates a WebSocket object.
 
         """
-        def default_factory():
-            return deque(maxlen=BUFFER_SIZE)
-        self.databuffer = defaultdict(default_factory)
-        self.messageRetriever = DataRetriever()
-
+        print 'SOCKET OPENED'
         self.msgSize = None
-        self.nSpecInAve = None
-        self.specAve = None # running average of spectra
-        self.publisher_socket = None
-        self.active_banks = None
 
-        # send message to client about what banks are active
-        self.banks = {'A':True, 'B':True, 'C':True, 'D':True, 'E':True, 'F':True, 'G':True, 'H':True}
-
-        for bank in self.banks.keys():
-            context = zmq.Context()
-            if LIVETEST:
-                major_key = "VEGAS"
-                minor_key = "Bank{0}Mgr".format(bank)
-            else:
-                major_key = "VegasTest"
-                minor_key = major_key + bank
-
-            stateKey = "%s.%s:P:state" % (major_key, minor_key)
-
-            directory_url = get_directory_endpoints("request")
-            device_url = get_service_endpoints(context, directory_url,
-                                               major_key, minor_key, SERV_SNAPSHOT)
-
-            request_url = context.socket(zmq.REQ)
-            if 'NOT FOUND' in device_url:
-                print 'Bank', bank, device_url
-                self.banks[bank] = False
-        
-        self.active_banks = [b for b in self.banks if self.banks[b]]
-        print 'Active banks',self.active_banks
-        self.write_message(['bank_config', self.active_banks])
+        if not self.connections:
+            # Start service that reads spectra from manager
+            #
+            print 'starting display server'
+            t = threading.Thread(target=self.run_display_server)
+            self.server_thread.append(t)
+            self.server_thread[0].start()
+            print 'done'
+        self.connections.append(self)
+        print "Connections:", len(self.connections), self.connections
 
     def on_message(self, waterfall_bank):
         """Handle message from client.
@@ -212,33 +291,14 @@ class ZMQWebSocket(websocket.WebSocketHandler):
         the bank_config step in Display.js.
 
         """
-        print '------------'
-        print 'Requesting BANK', waterfall_bank
-        print '------------'
+        print 'GOT A MESSAGE FROM THE CLIENT!', self
 
-        all_banks_spectra = []
-        metadata = []
-        pids = [] # process ids for parallel processing
-        result_queue = multiprocessing.Queue()
-        for b in self.banks:
-#            if self.banks[b]:
-#                p = multiprocessing.Process(target=self.messageRetriever.get_data_sample,
-#                                            args=(b, self,))
-#                pids.append(p)
-            response = self.messageRetriever.get_data_sample(b, self)
-            if response[0] == 'ok':
-                spectrum = response[1]
-                if b == waterfall_bank:
-                    project, scan, state, integration = response[2:]
-                    metadata = [project, scan, state, integration]
-            else:
-                spectrum = np.zeros(NCHANS)
-#            else:
-#                spectrum = np.zeros(NCHANS)
-
-            all_banks_spectra.append(spectrum)
-
-        self.write_message(['data', waterfall_bank, metadata, all_banks_spectra])
+        if self.data[0]:
+            print 'we have data', self
+            metadata = self.data[0][0]
+            spectra = self.data[0][1]
+            message = ['data', str(waterfall_bank), metadata, spectra]
+            self.write_message(message)
 
     def write_message(self, msg):
         """Send a message to the client.
@@ -252,6 +312,9 @@ class ZMQWebSocket(websocket.WebSocketHandler):
         get_data_sample() or socket open() on socket creation.
 
         """
+        if DEBUG:
+            print 'GETTING READY TO RESPOND'
+
         if not msg:
             print 'There is no message to write.  Quitting.'
             sys.exit()
@@ -259,16 +322,12 @@ class ZMQWebSocket(websocket.WebSocketHandler):
         elif 'data' == msg[0]:
             command_to_client = msg[0]
             waterfall_bank = str(msg[1])
-            metadata =  msg[2] # proj, scan, state, integration
+            metadata =  msg[2] # proj, scan, state, integration, update_waterfall
             all_banks_spectra = msg[3]
-
-            # self.databuffer[bank].append(spectrum)
 
             rebinned_spectra = []
             for spectrum in all_banks_spectra:
-                rebinned_spectrum = [np.mean(x) 
-                                     for x 
-                                     in spectrum.reshape((NCHANS,len(spectrum)/NCHANS))]
+                rebinned_spectrum = spectrum.reshape((NCHANS,len(spectrum)/NCHANS)).mean(axis=1).tolist()
                 rebinned_spectra.append(rebinned_spectrum)
 
             colormin = np.floor(min(rebinned_spectra[ BANK_NUM[waterfall_bank] ]))
@@ -276,7 +335,8 @@ class ZMQWebSocket(websocket.WebSocketHandler):
             color_limits = [colormin, colormax]
 
             data_to_send = [command_to_client, waterfall_bank, metadata, color_limits, rebinned_spectra]
-            print 'sending', command_to_client, waterfall_bank, metadata, color_limits
+            if DEBUG:
+                print 'sending', command_to_client, waterfall_bank, metadata, color_limits
 
             if DEBUG:
                 for idx,dd in enumerate(data_to_send):
@@ -303,10 +363,15 @@ class ZMQWebSocket(websocket.WebSocketHandler):
         super(ZMQWebSocket, self).write_message(data)
 
     def on_close(self):
+        self.connections.pop()
+        if not self.connections:
+            self.stop_requesting()
+            self.server_thread[0].join()
+            self.server_thread.pop()
         print "WebSocket closed"
+        print "Connections:", len(self.connections)
 
-if __name__ == "__main__":
-
+def listen_for_display_clients(port_number):
     settings = {
         "static_path": os.path.join(os.path.dirname(__file__), "static"),
     }
@@ -316,9 +381,21 @@ if __name__ == "__main__":
         (r"/websocket", ZMQWebSocket)
     ], **settings)
 
-    application.listen(8889,'0.0.0.0')
+    print 'add listener'
+    application.listen(port_number,'0.0.0.0')
     try:
+        print 'start ioloop listenting'
         tornado.ioloop.IOLoop.instance().start()
+        print 'left ioloop'
     except(KeyboardInterrupt):
         sys.exit()
+
+
+if __name__ == "__main__":
+
+    # Handle requests from clients to pass data from the stream
+    #
+    listen_for_display_clients(8888)
+    
+
 
