cb9ff3f12716698758ea800b5be2c041b2dcb6e7
Joe Masters <jmasters@nrao.edu>
2016-06-22 10:06:53 -0400

    Made control scripts more robust.  Add DISPLAY number as parameter.
    Now when the plot scripts are invoked, they should pass in the
    display number as the single paramter at the command line.
    
    For example, ./start_spectrum_plots.sh 1
    when we want to use DISPLAY=:1

--- controlscripts/start_plots.sh
+++ controlscripts/start_plots.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-source /home/gbt/gbt.bash
-source /home/gbt7/newt/McPython.bash
-source /home/gbt7/vegas_display/bin/activate
-
 fullpath=`readlink -f $0`
 pathdir=`dirname $fullpath`
 
--- controlscripts/start_waterfall_plots.sh
+++ controlscripts/start_waterfall_plots.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-source /home/gbt/gbt.bash
-source /home/gbt7/newt/McPython.bash
-source /home/gbt7/vegas_display/bin/activate
+# define the display number from a command line argument
+export DISPLAY=:$1
+
+# define a function to check the existence of a file
+#  and source it if it exists
+source_file()
+{
+    if test -e $1; then
+	source $1
+    else
+	echo "Can not find $1. Exiting."
+	exit 1
+    fi
+}
+
+source_file /home/gbt/gbt.bash
+source_file /home/gbt7/newt/McPython.bash
+source_file /home/gbt7/vegas_display/bin/activate
 
 fullpath=`readlink -f $0`
 pathdir=`dirname $fullpath`
